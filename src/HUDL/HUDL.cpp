e2d56ee8c9213acffc366e5b5227c2f79b90b367
Aaron Chan <aarchan108@gmail.com>
2022-09-08 20:16:38 -0400

    docstring update

--- src/HUDL/HUDL.cpp
+++ src/HUDL/HUDL.cpp
@@ -35,7 +35,7 @@ public:
 };
 
 /**
- *
+ * Write data
  * @param data
  */
 void HUDL::data_write(unsigned char data)  {
@@ -48,7 +48,7 @@ void HUDL::data_write(unsigned char data)  {
 }
 
 /**
- *
+ * Write command
  * @param data is the data being written for the command
  */
 void HUDL::comm_write(unsigned char data) {
