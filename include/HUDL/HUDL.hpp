388e61b651266cc953dc78c77c5d4998ab5ffcb9
Aaron Chan <aarchan108@gmail.com>
2022-10-24 19:33:21 -0400

    Wipe most of the code since its in EVT-Core's LCD class

--- include/HUDL/HUDL.hpp
+++ include/HUDL/HUDL.hpp
@@ -16,44 +16,9 @@ public:
     */
     HUDL(IO::GPIO& reg_select, IO::GPIO& reset, IO::GPIO& cs, IO::SPI& spi);
 
-    /**
-   * Writes data to the LCD to show on the screen
-     *
-   * @param[in] data being written to LCD
-   */
-    void data_write(unsigned char data);
-
-    /**
-    * Writes commands to the LCD to control the ST7565
-     *
-    * @param data being written for the command
-    */
-    void comm_write(unsigned char data);
-
-    /**
-    * Writes data to a single pixel
-     *
-    * @param[in] page is the page address to write data to
-    * @param[in] col_up is the first four bits of the column write
-    * @param[in] col_low is the last four bits of the column write
-    * @param[in] data is the data value to write
-    */
-    void drive_pixel(unsigned char page, unsigned char col_up,
-                     unsigned char col_low, unsigned char data);
-
-    /**
-    * Clears the screen
-     *
-    * @param[in] lcd_string
-    */
-    void clear_lcd(unsigned char* lcd_string);
-
-    /**
-     * Initializes LCD for use
-     */
-    void init_LCD();
 
 private:
+    // TODO: These pins will go into an LCD class which will be a field instead
     IO::GPIO& reg_select;// PA_3
     IO::GPIO& reset;     // PB_3
     IO::GPIO& cs;        // PB_12
--- src/HUDL/HUDL.cpp
+++ src/HUDL/HUDL.cpp
@@ -11,86 +11,9 @@
 // clang-format on
 
 namespace IO = EVT::core::IO;
-
 namespace HUDL {
 HUDL::HUDL(IO::GPIO& reg_select, IO::GPIO& reset, IO::GPIO& cs, IO::SPI& spi)
     : reg_select(reg_select), reset(reset), cs(cs), spi(spi) {}
 
-void HUDL::data_write(unsigned char data) {
-    cs.writePin(EVT::core::IO::GPIO::State::LOW);
-    reg_select.writePin(EVT::core::IO::GPIO::State::HIGH);
-    spi.startTransmission(0);
-    spi.write(&data, 1);
-    spi.endTransmission(0);
-    cs.writePin(EVT::core::IO::GPIO::State::HIGH);
-}
-
-void HUDL::comm_write(unsigned char data) {
-    cs.writePin(EVT::core::IO::GPIO::State::LOW);
-    reg_select.writePin(EVT::core::IO::GPIO::State::LOW);
-    spi.startTransmission(0);
-    spi.write(&data, 1);
-    spi.endTransmission(0);
-    cs.writePin(EVT::core::IO::GPIO::State::HIGH);
-}
-
-void HUDL::drive_pixel(unsigned char page, unsigned char col_up,
-                       unsigned char col_low, unsigned char data) {
-    HUDL::comm_write(
-        0x40);// line to start writing on (0 -> 64) moves set bits with it
-    // DO NOT CHANGE
-    comm_write(
-        0xB0 + page);// writes the page address (4 bits, 8 rows selected by values 0-7 )
-    comm_write(
-        0x10 + col_up);        // writes the first 4 bits of the column select (out of 8 bits)
-    comm_write(0x00 + col_low);// writes the second 4 bits of the column select (out)
-
-    data_write(data);// writes 8 vertical bits based on value between 0-255 based
-                     // on bits set ex: 01001100 is       |WHITE|
-                     //  |BLACK|
-                     //  |WHITE|
-                     //  |WHITE|
-                     //  |BLACK|
-                     //  |BLACK|
-                     //  |WHITE|
-                     //  |WHITE|
-}
-
-void HUDL::clear_lcd(unsigned char* lcd_string) {
-    unsigned char page = 0xB0;
-    comm_write(0xAE);// Display OFF
-    comm_write(0x40);// Display start address + 0x40
-
-    // 64 pixel display / 8 pixels per page = 8 pages
-    for (int i = 0; i < 8; i++) {
-        comm_write(page);// send page address
-        comm_write(0x10);// column address upper 4 bits + 0x10
-        comm_write(0x00);// column address lower 4 bits + 0x00
-
-        // 128 columns wide
-        for (int j = 0; j < 128; j++) {
-            data_write(0x00);// write clear pixels
-            lcd_string++;
-        }
-
-        page++;// after 128 columns, go to next page
-    }
-
-    comm_write(0xAF);
-}
-
-void HUDL::init_LCD() {
-    HUDL::comm_write(0xA0);// ADC select
-    HUDL::comm_write(0xAE);// Display OFF
-    HUDL::comm_write(0xC8);// COM direction scan
-    HUDL::comm_write(0xA2);// LCD bias set
-    HUDL::comm_write(0x2F);// Power Control set
-    HUDL::comm_write(0x26);// Resistor Ratio Set
-    HUDL::comm_write(
-        0x81);// Electronic Volume Command (set contrast) Double Byte: 1 of 2
-    HUDL::comm_write(
-        0x11);             // Electronic Volume value (contrast value) Double Byte: 2 of 2
-    HUDL::comm_write(0xAF);// Display ON
-}
 
 }// namespace HUDL
