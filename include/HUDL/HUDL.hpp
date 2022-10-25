99770291294b7ed9c74509df93835c54dc05cc68
Aaron Chan <aarchan108@gmail.com>
2022-10-24 20:16:25 -0400

    Fix documentation formatting changed by GitHub

--- include/HUDL/HUDL.hpp
+++ include/HUDL/HUDL.hpp
@@ -13,57 +13,57 @@ namespace HUDL {
 class HUDL {
 public:
     /**
-        * Default Constructor for the HUDL class
-         *
-        * @param[in] reg_select is the register select pin
-        * @param[in] reset is the reset pin
-        * @param[in] cs is the chip select pin
-        * @param[in] spi is the SPI instance
-        */
+    * Default Constructor for the HUDL class
+    *
+    * @param[in] reg_select is the register select pin
+    * @param[in] reset is the reset pin
+    * @param[in] cs is the chip select pin
+    * @param[in] spi is the SPI instance
+    */
     HUDL(IO::GPIO& reg_select, IO::GPIO& reset, IO::GPIO& cs, IO::SPI& spi);
 
     /**
-       * Writes data to the LCD to show on the screen
-         *
-       * @param[in] data being written to LCD
-       */
+     * Writes data to the LCD to show on the screen
+     *
+     * @param[in] data being written to LCD
+     */
     void dataWrite(uint8_t data);
 
     /**
-        * Writes commands to the LCD to control the ST7565
-         *
-        * @param data being written for the command
-        */
+    * Writes commands to the LCD to control the ST7565
+    *
+    * @param data being written for the command
+    */
     void commWrite(uint8_t data);
 
     /**
-        * Writes data to a single pixel
-         *
-        * @param[in] page is the page address to write data to
-        * @param[in] colUp is the first four bits of the column write
-        * @param[in] colLow is the last four bits of the column write
-        * @param[in] data is the data value to write
-        */
+    * Writes data to a single pixel
+    *
+    * @param[in] page is the page address to write data to
+    * @param[in] colUp is the first four bits of the column write
+    * @param[in] colLow is the last four bits of the column write
+    * @param[in] data is the data value to write
+    */
     void drivePixel(uint8_t page, uint8_t colUp, uint8_t colLow, uint8_t data);
 
     /**
-        * Clears the screen
-         *
-        * @param[in] lcd_string
-        */
+    * Clears the screen
+    *
+    * @param[in] lcd_string
+    */
     void clearLCD(const uint8_t* lcd_string);
 
     /**
-         * Initializes LCD for use
-         */
+     * Initializes LCD for use
+     */
     void initLCD();
 
 private:
-    // TODO: Remove later. Commented out since it is used for LCD now
-    //    IO::GPIO& reg_select;// PA_3
-    //    IO::GPIO& reset;     // PB_3
-    //    IO::GPIO& cs;        // PB_12
-    //    IO::SPI& spi;
+    /**
+     * reg_select PA_3
+     * reset      PB_3
+     * cs         PB_12
+     */
 
     DEV::LCD lcd;
 };
