1cce51e38bee82eb3e9ab1f90a50c9f5520d87c0
Aaron Chan <aarchan108@gmail.com>
2022-10-27 19:04:42 -0400

    Externs to fix linker issues

--- targets/HUDL/main.cpp
+++ targets/HUDL/main.cpp
@@ -13,9 +13,36 @@
 #include <EVT/utils/time.hpp>
 #include <HUDL/HUDL.hpp>
 #include <EVT/dev/Timer.hpp>
-#include <HALf3/stm32f3xx_hal_tim.h>
 #include <EVT/dev/platform/f3xx/f302x8/Timerf302x8.hpp>
 #include "Canopen/co_core.h"
+#include "Canopen/co_tmr.h"
+
+extern "C" void CONodeFatalError(void) {
+}
+
+extern "C" void COIfCanReceive(CO_IF_FRM* frm) {}
+
+extern "C" int16_t COLssStore(uint32_t baudrate, uint8_t nodeId) { return 0; }
+
+extern "C" int16_t COLssLoad(uint32_t* baudrate, uint8_t* nodeId) { return 0; }
+
+extern "C" void CONmtModeChange(CO_NMT* nmt, CO_MODE mode) {}
+
+extern "C" void CONmtHbConsEvent(CO_NMT* nmt, uint8_t nodeId) {}
+
+extern "C" void CONmtHbConsChange(CO_NMT* nmt, uint8_t nodeId, CO_MODE mode) {}
+
+extern "C" int16_t COParaDefault(CO_PARA* pg) { return 0; }
+
+extern "C" void COPdoTransmit(CO_IF_FRM* frm) {}
+
+extern "C" int16_t COPdoReceive(CO_IF_FRM* frm) { return 0; }
+
+extern "C" void COPdoSyncUpdate(CO_RPDO* pdo) {}
+
+extern "C" void COTmrLock(void) {}
+
+extern "C" void COTmrUnlock(void) {}
 
 
 namespace IO = EVT::core::IO;
@@ -32,7 +59,7 @@ const uint8_t deviceCount = 1;
  * to the interrupt handler.
  */
 struct CANInterruptParams {
-    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>* queue;
+    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> *queue;
 };
 
 /**
@@ -40,10 +67,10 @@ struct CANInterruptParams {
  *
  * @param priv[in] The private data (FixedQueue<CANOPEN_QUEUE_SIZE, CANMessage>)
  */
-void canInterruptHandler(IO::CANMessage& message, void* priv) {
-    struct CANInterruptParams* params = (CANInterruptParams*) priv;
+void canInterruptHandler(IO::CANMessage &message, void *priv) {
+    struct CANInterruptParams *params = (CANInterruptParams *) priv;
 
-    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>* queue =
+    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> *queue =
             params->queue;
 
     if (queue == nullptr)
@@ -53,30 +80,30 @@ void canInterruptHandler(IO::CANMessage& message, void* priv) {
 }
 
 int main() {
-    IO::GPIO* devices[deviceCount];
-    auto& reg_select = IO::getGPIO<IO::Pin::PA_3>(IO::GPIO::Direction::OUTPUT);
+    IO::GPIO *devices[deviceCount];
+    auto &reg_select = IO::getGPIO<IO::Pin::PA_3>(IO::GPIO::Direction::OUTPUT);
     reg_select.writePin(IO::GPIO::State::LOW);
 
-    auto& reset = IO::getGPIO<IO::Pin::PB_3>(IO::GPIO::Direction::OUTPUT);
+    auto &reset = IO::getGPIO<IO::Pin::PB_3>(IO::GPIO::Direction::OUTPUT);
     reset.writePin(IO::GPIO::State::LOW);
     time::wait(100);
     reset.writePin(IO::GPIO::State::HIGH);
     time::wait(100);
 
-    auto& cs = IO::getGPIO<IO::Pin::PB_12>(IO::GPIO::Direction::OUTPUT);
+    auto &cs = IO::getGPIO<IO::Pin::PB_12>(IO::GPIO::Direction::OUTPUT);
     cs.writePin(IO::GPIO::State::HIGH);
 
-    auto& hudl_spi = IO::getSPI<IO::Pin::PB_13, IO::Pin::PB_15, IO::Pin::PC_11>(
-        devices, deviceCount);
+    auto &hudl_spi = IO::getSPI<IO::Pin::PB_13, IO::Pin::PB_15, IO::Pin::PC_11>(
+            devices, deviceCount);
     hudl_spi.configureSPI(SPI_SPEED, SPI_MODE3, SPI_MSB_FIRST);
 
     auto hudl = HUDL::HUDL(reg_select, reset, cs, hudl_spi);
 
     // Setup UART
-    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+    IO::UART const &uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
 
     // Setup CAN
-    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>(); // TODO: Figure out CAN pins
+    IO::CAN &can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>(); // TODO: Figure out CAN pins
     EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> canOpenQueue;
     DEV::Timerf302x8 timer(TIM2, 100);
     // TODO: Need a CAN interrupt handler
