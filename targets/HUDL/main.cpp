a7af54ac33b689d0e8e083773c4f16ab1be9d7b2
Aaron Chan <aarchan108@gmail.com>
2022-12-01 20:51:07 -0500

    Delete commented code

--- targets/HUDL/main.cpp
+++ targets/HUDL/main.cpp
@@ -98,7 +98,6 @@ int main() {
     // Initialize system
     IO::init();
 
-    uart.printf("Fuck you\n\r");
     // Will store CANopen messages that will be populated by the EVT-core CAN
     // interrupt
     EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> canOpenQueue;
