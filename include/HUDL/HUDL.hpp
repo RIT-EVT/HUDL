0947426273f37ee7ce0380f24083d5a417477c9a
Aaron Chan <aarchan108@gmail.com>
2022-12-09 15:11:48 -0500

    Testing comment block fix

--- include/HUDL/HUDL.hpp
+++ include/HUDL/HUDL.hpp
@@ -15,85 +15,85 @@ namespace DEV = EVT::core::DEV;
 namespace HUDL {
 class HUDL {
 public:
-    /**
-        * The node ID used to identify the device on the CAN network.
-        */
+/**
+* The node ID used to identify the device on the CAN network.
+*/
     static constexpr uint8_t NODE_ID = 0x11;
 
     /**
-         * Default Constructor for the HUDL class
-         *
-         * @param[in] reg_select is the register select pin
-         * @param[in] reset is the reset pin
-         * @param[in] cs is the chip select piarrayn
-         * @param[in] spi is the SPI instance
-         */
+* Default Constructor for the HUDL class
+*
+* @param[in] reg_select is the register select pin
+* @param[in] reset is the reset pin
+* @param[in] cs is the chip select piarrayn
+* @param[in] spi is the SPI instance
+*/
     HUDL(IO::GPIO& reg_select, IO::GPIO& reset, IO::SPI& spi);
 
     /**
-         * Writes data to the LCD to show on the screen
-         *
-         * @param[in] data being written to LCD
-         */
+* Writes data to the LCD to show on the screen
+*
+* @param[in] data being written to LCD
+*/
     void dataWrite(uint8_t data);
 
-    /**
-         * Writes commands to the LCD to control the ST7565
-         *
-         * @param data being written for the command
-         */
+/**
+* Writes commands to the LCD to control the ST7565
+*
+* @param data being written for the command
+*/
     void commWrite(uint8_t data);
 
     /**
-         * Writes data to a single pixel
-         *
-         * @param[in] page is the page address to write data to
-         * @param[in] colUp is the first four bits of the column write
-         * @param[in] colLow is the last four bits of the column write
-         * @param[in] data is the data value to write
-         */
+* Writes data to a single pixel
+*
+* @param[in] page is the page address to write data to
+* @param[in] colUp is the first four bits of the column write
+* @param[in] colLow is the last four bits of the column write
+* @param[in] data is the data value to write
+*/
     void drivePixel(uint8_t page, uint8_t colUp, uint8_t colLow, uint8_t data);
 
     /**
-         * Clears the screen
-         *
-         * @param[in] bitMap a pointer to the bitmap to be displayed
-         */
+* Clears the screen
+*
+* @param[in] bitMap a pointer to the bitmap to be displayed
+*/
     void clearLCD(const uint8_t* bitMap);
 
     /**
-         * Initializes LCD for use
-         */
+* Initializes LCD for use
+*/
     void initLCD();
 
     /**
-         * Gets the object dictionary
-         *
-         * @return an object dictionary
-         */
+* Gets the object dictionary
+*
+* @return an object dictionary
+*/
     CO_OBJ_T* getObjectDictionary();
 
     /**
-         * Gets the size of the Object Dictionary
-         *
-         * @return uint16_t size of the Object Dictionary
-         */
+ * Gets the size of the Object Dictionary
+ *
+ * @return uint16_t size of the Object Dictionary
+ */
     uint16_t getObjectDictionarySize() const;
 
     void displayMap(uint8_t* bitmap);
 
     /**
-         * Gets BMS Voltage values
-         *
-         * @return uint16_t* pointer to voltage values
-         */
+* Gets BMS Voltage values
+*
+* @return uint16_t* pointer to voltage values
+ */
     uint16_t* getVoltages();
 
     /**
-         * Gets temperature values
-         *
-         * @return uint32_t* pointer of temperature values
-         */
+* Gets temperature values
+*
+* @return uint32_t* pointer of temperature values
+*/
     uint32_t* getThermTemps();
 
 private:
@@ -110,14 +110,7 @@ private:
     static constexpr uint16_t OBJECT_DICTIONARY_SIZE = 30;
 
     CO_OBJ_T objectDictionary[OBJECT_DICTIONARY_SIZE + 1] = {
-        // Sync ID, defaults to 0x80
         {CO_KEY(0x1005, 0, CO_UNSIGNED32 | CO_OBJ_D__R_), 0, (uintptr_t) 0x80},
-
-        // Information about the hardware, hard coded sample values for now
-        // 1: Vendor ID
-        // 2: Product Code
-        // 3: Revision Number
-        // 4: Serial Number
         {
             .Key = CO_KEY(0x1018, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
@@ -139,9 +132,6 @@ private:
             .Data = (uintptr_t) 0x13,
         },
 
-        // SDO CAN message IDS.
-        // 1: Client -> Server ID, default is 0x600 + NODE_ID
-        // 2: Server -> Client ID, default is 0x580 + NODE_ID
         {
             .Key = CO_KEY(0x1200, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
@@ -153,54 +143,39 @@ private:
             .Data = (uintptr_t) 0x580 + NODE_ID,
         },
 
-        //RPDO settings
-        // 0: RPDO number in index and total number of sub indexes.
-        // 1: The COB-ID to receive PDOs from.
-        // 2: transmission trigger
+
         {
             .Key = CO_KEY(0x1400, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 3,
         },
         {
-            // 180h+TPDO Node-ID
             .Key = CO_KEY(0x1400, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0),
         },
         {
-            // asynchronous trigger
             .Key = CO_KEY(0x1400, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 0xFE,
         },
 
-        //RPDO1 settings
-        // 0: RPDO number in index and total number of sub indexes.
-        // 1: The COB-ID to receive PDOs from.
-        // 2: transmission trigger
         {
             .Key = CO_KEY(0x1401, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 3,
         },
         {
-            // 180h+TPDO Node-ID
             .Key = CO_KEY(0x1401, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(1),
         },
         {
-            // asynchronous trigger
             .Key = CO_KEY(0x1401, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
             .Data = (uintptr_t) 0xFE,
         },
 
-        // RPDO0 mapping, determines the PDO messages to send when RPDO1 is triggered
-        // 0: The number of PDO message associated with the RPDO
-        // 1: Link to the first PDO message
-        // n: Link to the nth PDO message
         {
             .Key = CO_KEY(0x1600, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
@@ -217,10 +192,6 @@ private:
             .Data = CO_LINK(0x2100, 1, 8),
         },
 
-        // RPDO1 mapping, determines the PDO messages to send when RPDO1 is triggered
-        // 0: The number of PDO message associated with the RPDO
-        // 1: Link to the first PDO message
-        // n: Link to the nth PDO message
         {
             .Key = CO_KEY(0x1601, 0, CO_UNSIGNED8 | CO_OBJ_D__R_),
             .Type = nullptr,
@@ -237,8 +208,6 @@ private:
             .Data = CO_LINK(0x2100, 3, 8),
         },
 
-        // User defined data, this will be where we put elements that can be
-        // accessed via SDO and depending on configuration PDO
         {
             .Key = CO_KEY(0x2100, 0, CO_UNSIGNED32 | CO_OBJ___PRW),
             .Type = nullptr,
