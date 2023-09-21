f553b53576c2890a78aac199eb9f2bf8fdea1b57
ActuallyTaylor <zachary.lineman@gmail.com>
2023-09-21 19:38:31 -0400

    Fixed HUDL not having UART and receiving data
    Signed-off-by: ActuallyTaylor <zachary.lineman@gmail.com>

--- src/HUDL/HUDL.cpp
+++ src/HUDL/HUDL.cpp
@@ -25,11 +25,6 @@ HUDL::HUDL(IO::GPIO& reg_select, IO::GPIO& reset, IO::SPI& spi) : lcd(DEV::LCD(r
 void HUDL::initLCD() {
     lcd.initLCD();
     lcd.clearLCD();
-    lcd.setEntireScreenBitMap(evtBitMap);
-    EVT::core::time::wait(2000);
-    lcd.clearLCD();
-    //    lcd.setDefaultSections(SECTION_TITLES);
-    //    lcd.displaySectionHeaders();
 }
 
 CO_OBJ_T* HUDL::getObjectDictionary() {
@@ -41,62 +36,49 @@ uint16_t HUDL::getObjectDictionarySize() const {
 }
 
 void HUDL::updateLCD() {
-    switch (currentHUDLScreen) {
-    case PAGE_1: {// Set a new scope for this case
-        if (!setHeaders) {
-            lcd.clearLCD();
-            headerForCorner(TOP_LEFT, "Battery");
-            headerForCorner(TOP_RIGHT, "HI Temp");
-            headerForCorner(BOTTOM_LEFT, "RPM");
-            headerForCorner(BOTTOM_RIGHT, "MC Stat");
-            setHeaders = true;
-        }
-
-        // Set the battery voltage
-        char voltage[9];
-        std::sprintf(voltage, "%hu.%hu v", totalVoltage / 10, totalVoltage % 10);
-        dataForCorner(TOP_LEFT, voltage);
-
-        // Set the highest temp
-        uint16_t highestTemp = thermTemps[0];
-        for (uint16_t temp : thermTemps) {
-            if (temp > highestTemp) {
-                highestTemp = temp;
-            }
-        }
-        char temp[9];
-        std::sprintf(temp, "%hu.%hu C", thermTemps[0] / 100, thermTemps[0] % 100);
-
-        dataForCorner(TOP_RIGHT, temp);
-
-        // Set the rpm
-        char rpm[8];
-        std::sprintf(rpm, "%lu", actualPosition);
-        dataForCorner(BOTTOM_LEFT, rpm);
-
-        // Set the status word
-        char status[8];
-
-        if (statusWord == 0x21) {
-            std::sprintf(status, "STOP");
-        } else if (statusWord == 0x27) {
-            std::sprintf(status, "GO");
-        } else {
-            std::sprintf(status, "UNKNOWN");
-            //            std::sprintf(errorString, "There was an error with the Motor Controller: Unknown Error %x", statusWord);
-            //            currentHUDLScreen = ERROR_PAGE;
-        }
-
-        dataForCorner(BOTTOM_RIGHT, status);
-    } break;
-    case ERROR_PAGE: {// Set a new scope for this page
-        if (!setHeaders) {
-            lcd.clearLCD();
-        }
-        writeError(errorString);
-        break;
+    if (!setHeaders) {
+        lcd.clearLCD();
+        headerForCorner(TOP_LEFT, "Battery");
+        headerForCorner(TOP_RIGHT, "HI Temp");
+        headerForCorner(BOTTOM_LEFT, "RPM");
+        headerForCorner(BOTTOM_RIGHT, "MC Stat");
+        setHeaders = true;
     }
+
+    // Set the battery voltage
+    char voltage[9];
+    std::sprintf(voltage, "%hu.%hu v", totalVoltage / 10, totalVoltage % 10);
+    dataForCorner(TOP_LEFT, voltage);
+
+    // Set the highest temp
+    uint16_t highestTemp = thermTemps[0];
+    for (uint16_t temp : thermTemps) {
+        if (temp > highestTemp) {
+            highestTemp = temp;
+        }
     }
+
+    char temp[9];
+    std::sprintf(temp, "0x%02X C", highestTemp);
+    dataForCorner(TOP_RIGHT, temp);
+
+    // Set the rpm
+    char rpm[8];
+    std::sprintf(rpm, "%u", actualPosition);
+    dataForCorner(BOTTOM_LEFT, rpm);
+
+    // Set the status word
+    char status[8];
+
+    if (statusWord == 0x21) {
+        std::sprintf(status, "STOP");
+    } else if (statusWord == 0x27) {
+        std::sprintf(status, "GO");
+    } else {
+        std::sprintf(status, "0x%x", statusWord);
+    }
+
+    dataForCorner(BOTTOM_RIGHT, status);
 }
 
 void HUDL::headerForCorner(HUDL::Corner corner, const char* text) {
@@ -113,7 +95,7 @@ void HUDL::headerForCorner(HUDL::Corner corner, const char* text) {
     sectionColumn += padding;
 
     // Write the text to the screen under the section header.
-    lcd.writeLargeText(text, sectionPage, sectionColumn, false);
+    lcd.writeText(text, sectionPage, sectionColumn, EVT::core::DEV::LCD::FontSize::LARGE, false);
 }
 
 void HUDL::dataForCorner(HUDL::Corner corner, const char* text) {
@@ -130,21 +112,12 @@ void HUDL::dataForCorner(HUDL::Corner corner, const char* text) {
     sectionColumn += padding;
 
     // Write the text to the screen under the section header.
-    lcd.writeLargeText(text, sectionPage, sectionColumn, false);
+    lcd.writeText(text, sectionPage, sectionColumn, EVT::core::DEV::LCD::FontSize::LARGE, false);
 }
 
-void HUDL::writeError(const char* text) {
-    // Calculate the padding to center the text in the section
-    uint8_t length = strlen("!!! Error !!!") * 8;
-    uint8_t padding = (128 - length) / 2;
-
-    // Write the Error header
-    lcd.writeLargeText("!!! Error !!!", 0, padding, false);
-
-    // Write the error message
-    lcd.writeSmallText(text, 2, 0, true);
-}
-
+// Ignore the -Wreturn-type warning that occurs here because of the switch statements.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreturn-type"
 uint8_t HUDL::columnForCorner(HUDL::Corner corner) {
     switch (corner) {
     case TOP_LEFT:
@@ -153,17 +126,6 @@ uint8_t HUDL::columnForCorner(HUDL::Corner corner) {
     case TOP_RIGHT:
     case BOTTOM_RIGHT:
         return 64;
-    }
-}
-
-uint8_t HUDL::wrapForCorner(HUDL::Corner corner) {
-    switch (corner) {
-    case TOP_LEFT:
-    case BOTTOM_LEFT:
-        return 64;
-    case TOP_RIGHT:
-    case BOTTOM_RIGHT:
-        return 128;
     }
 }
 
@@ -177,4 +139,6 @@ uint8_t HUDL::pageForCorner(HUDL::Corner corner) {
         return 4;
     }
 }
+#pragma GCC diagnostic pop
+
 }// namespace HUDL
--- targets/HUDL/main.cpp
+++ targets/HUDL/main.cpp
@@ -21,12 +21,12 @@ namespace time = EVT::core::time;
 namespace log = EVT::core::log;
 using namespace std;
 
-const uint32_t SPI_SPEED = SPI_SPEED_500KHZ;
-const uint8_t deviceCount = 1;
+#define SPI_SPEED SPI_SPEED_500KHZ
+#define DEVICE_COUNT 1
 
 ///////////////////////////////////////////////////////////////////////////////
-// EVT-core CAN callback and CAN setup. This will include logic to set
-// aside CANopen messages into a specific queue
+// EVT-core CAN call back and CAN set up. This will include logic to set     //
+// aside CANOpen messages into a specific queue                              //
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
@@ -42,21 +42,135 @@ const uint8_t deviceCount = 1;
 
 // create a can interrupt handler
 void canInterrupt(IO::CANMessage& message, void* priv) {
-    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>* queue =
-        (EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>*) priv;
+    auto* queue = (EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>*) priv;
 
-    //    // Log raw received data
-    //    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "Got RAW message from %X of length %d with data: ", message.getId(), message.getDataLength());
-    //
-    //    uint8_t* data = message.getPayload();
-    //    for (int i = 0; i < message.getDataLength(); i++) {
-    //        log::LOGGER.log(log::Logger::LogLevel::DEBUG, "%X ", *data);
-    //        data++;
-    //    }
     if (queue != nullptr) {
         queue->append(message);
     }
 }
+
+int main() {
+    // Initialize system
+    EVT::core::platform::init();
+
+    IO::UART& uart = IO::getUART<IO::Pin::PB_9, IO::Pin::PB_8>(9600);
+    log::LOGGER.setUART(&uart);
+
+    // Will store CANOpen messages that will be populated by the EVT-core CAN
+    // interrupt
+    auto canOpenQueue = EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>(true);
+
+    // Initialize CAN, add an IRQ which will add messages to the queue above
+    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
+    can.addIRQHandler(canInterrupt, reinterpret_cast<void*>(&canOpenQueue));
+
+    // Initialize the timer
+    DEV::Timerf3xx timer(TIM2, 160);
+
+    //create the RPDO node
+    IO::GPIO* devices[DEVICE_COUNT];
+
+    IO::GPIO& regSelect = IO::getGPIO<IO::Pin::PA_3>(EVT::core::IO::GPIO::Direction::OUTPUT);
+
+    IO::GPIO& reset = IO::getGPIO<IO::Pin::PB_7>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[0] = &IO::getGPIO<IO::Pin::PB_12>(EVT::core::IO::GPIO::Direction::OUTPUT);
+    devices[0]->writePin(IO::GPIO::State::HIGH);
+
+    auto& hudl_spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI>(devices, DEVICE_COUNT);
+
+    IO::PWM& brightness = IO::getPWM<IO::Pin::PC_0>();
+    brightness.setPeriod(1);
+    brightness.setDutyCycle(100);
+
+    hudl_spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
+
+    HUDL::HUDL hudl(regSelect, reset, hudl_spi);
+
+    // Reserved memory for CANopen stack usage
+    uint8_t sdoBuffer[1][CO_SDO_BUF_BYTE];
+    CO_TMR_MEM appTmrMem[4];
+
+    // Attempt to join the CAN network
+    IO::CAN::CANStatus result = can.connect();
+
+    // Adds CAN filtering to only allow messages from IDs 1, 5, and 8.
+    can.addCANFilter(0x1, 0b00001111111, 0);
+    can.addCANFilter(0x8, 0b00001111111, 1);
+    can.addCANFilter(0x5, 0b00001111111, 2);
+
+    //test that the board is connected to the can network
+    if (result != IO::CAN::CANStatus::OK) {
+        log::LOGGER.log(log::Logger::LogLevel::ERROR, "Failed to connect to CAN network\r\n");
+        return 1;
+    } else {
+        log::LOGGER.log(log::Logger::LogLevel::INFO, "Connected to CAN network\r\n");
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Setup CAN configuration, this handles making drivers, applying settings.
+    // And generally creating the CANopen stack node which is the interface
+    // between the application (the code we write) and the physical CAN network
+    ///////////////////////////////////////////////////////////////////////////
+    // Make drivers
+    CO_IF_DRV canStackDriver;
+
+    CO_IF_CAN_DRV canDriver;
+    CO_IF_TIMER_DRV timerDriver;
+    CO_IF_NVM_DRV nvmDriver;
+
+    IO::getCANopenCANDriver(&can, &canOpenQueue, &canDriver);
+    IO::getCANopenTimerDriver(&timer, &timerDriver);
+    IO::getCANopenNVMDriver(&nvmDriver);
+
+    canStackDriver.Can = &canDriver;
+    canStackDriver.Timer = &timerDriver;
+    canStackDriver.Nvm = &nvmDriver;
+
+    //setup CANopen Node
+    CO_NODE_SPEC canSpec = {
+        .NodeId = HUDL::HUDL::NODE_ID,
+        .Baudrate = IO::CAN::DEFAULT_BAUD,
+        .Dict = hudl.getObjectDictionary(),
+        .DictLen = hudl.getObjectDictionarySize(),
+        .EmcyCode = nullptr,
+        .TmrMem = appTmrMem,
+        .TmrNum = 16,
+        .TmrFreq = 1,
+        .Drv = &canStackDriver,
+        .SdoBuf = reinterpret_cast<uint8_t*>(&sdoBuffer[0]),
+    };
+
+    CO_NODE canNode;
+
+    CONodeInit(&canNode, &canSpec);
+    CONodeStart(&canNode);
+    CONmtSetMode(&canNode.Nmt, CO_OPERATIONAL);
+
+    time::wait(500);
+
+    //print any CANOpen errors
+    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "Error: %d\r\n", CONodeGetErr(&canNode));
+
+    hudl.initLCD();
+    uint16_t displayCounter = 0;
+
+    while (true) {
+        if (displayCounter >= 1000) {
+            displayCounter = 0;
+
+            hudl.updateLCD();
+        } else {
+            displayCounter++;
+        }
+
+        CONodeProcess(&canNode);
+        // Update the state of timer based events
+        COTmrService(&canNode.Tmr);
+        // Handle executing timer events that have elapsed
+        COTmrProcess(&canNode.Tmr);
+    }
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // CANopen specific Callbacks. Need to be defined in some location
@@ -86,146 +200,3 @@ extern "C" void COPdoSyncUpdate(CO_RPDO* pdo) {}
 extern "C" void COTmrLock(void) {}
 
 extern "C" void COTmrUnlock(void) {}
-
-extern "C" void HAL_CAN_RxFifo1FullCallback(CAN_HandleTypeDef* hcan) {
-    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "RX Full");
-}
-
-int main() {
-    // Initialize system
-    EVT::core::platform::init();
-
-    // Will store CANopen messages that will be populated by the EVT-core CAN
-    // interrupt
-    auto canOpenQueue = EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>(true);
-
-    // Initialize CAN, add an IRQ which will add messages to the queue above
-    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
-    can.addIRQHandler(canInterrupt, reinterpret_cast<void*>(&canOpenQueue));
-
-    // Initialize the timer
-    DEV::Timerf3xx timer(TIM2, 160);
-
-    //create the RPDO node
-    IO::GPIO* devices[deviceCount];
-
-    IO::GPIO& regSelect = IO::getGPIO<IO::Pin::PA_3>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    // HUDL 1.0
-    //    IO::GPIO& reset = IO::getGPIO<IO::Pin::PB_3>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    //    devices[0] = &IO::getGPIO<IO::Pin::PB_12>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    // HUDL 1.1
-    //    IO::GPIO& reset = IO::getGPIO<IO::Pin::PB_7>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    //    devices[0] = &IO::getGPIO<IO::Pin::PB_11>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    // HUDL 1.2
-    IO::GPIO& reset = IO::getGPIO<IO::Pin::PB_7>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    devices[0] = &IO::getGPIO<IO::Pin::PB_12>(EVT::core::IO::GPIO::Direction::OUTPUT);
-
-    devices[0]->writePin(IO::GPIO::State::HIGH);
-    //    auto& hudl_spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI>(
-    //        devices, deviceCount);
-    auto& hudl_spi = IO::getSPI<IO::Pin::SPI_SCK, IO::Pin::SPI_MOSI>(
-        devices, deviceCount);
-
-    IO::PWM& brightness = IO::getPWM<IO::Pin::PC_0>();
-    brightness.setPeriod(1);
-    brightness.setDutyCycle(100);
-
-    /*
-    IO::GPIO& regSelect = IO::getGPIO<IO::Pin::PA_3>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    IO::GPIO& reset = IO::getGPIO<IO::Pin::PB_7>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    devices[0] = &IO::getGPIO<IO::Pin::PB_11>(EVT::core::IO::GPIO::Direction::OUTPUT);
-    devices[0]->writePin(IO::GPIO::State::HIGH);
-     */
-
-    hudl_spi.configureSPI(SPI_SPEED, SPI_MODE0, SPI_MSB_FIRST);
-
-    HUDL::HUDL hudl(regSelect, reset, hudl_spi);
-
-    // Reserved memory for CANopen stack usage
-    uint8_t sdoBuffer[1][CO_SDO_BUF_BYTE];
-    CO_TMR_MEM appTmrMem[4];
-
-    // Attempt to join the CAN network
-    IO::CAN::CANStatus result = can.connect();
-
-    // Adds can filtering to only allow messages from IDs 1, 5, and 8.
-    can.addCANFilter(0x1, 0b00001111111, 0);
-    can.addCANFilter(0x8, 0b00001111111, 1);
-    can.addCANFilter(0x5, 0b00001111111, 2);
-
-    //test that the board is connected to the can network
-    if (result != IO::CAN::CANStatus::OK) {
-        log::LOGGER.log(log::Logger::LogLevel::ERROR, "Failed to connect to CAN network\r\n");
-        return 1;
-    } else {
-        log::LOGGER.log(log::Logger::LogLevel::INFO, "Connected to CAN network\r\n");
-    }
-
-    // Crashes the HUDL
-    //    IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
-    //    log::LOGGER.setUART(&uart);
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Setup CAN configuration, this handles making drivers, applying settings.
-    // And generally creating the CANopen stack node which is the interface
-    // between the application (the code we write) and the physical CAN network
-    ///////////////////////////////////////////////////////////////////////////
-    // Make drivers
-    CO_IF_DRV canStackDriver;
-
-    CO_IF_CAN_DRV canDriver;
-    CO_IF_TIMER_DRV timerDriver;
-    CO_IF_NVM_DRV nvmDriver;
-
-    IO::getCANopenCANDriver(&can, &canOpenQueue, &canDriver);
-    IO::getCANopenTimerDriver(&timer, &timerDriver);
-    IO::getCANopenNVMDriver(&nvmDriver);
-
-    canStackDriver.Can = &canDriver;
-    canStackDriver.Timer = &timerDriver;
-    canStackDriver.Nvm = &nvmDriver;
-
-    //setup CANopen Node
-    CO_NODE_SPEC canSpec = {
-        .NodeId = HUDL::HUDL::NODE_ID,
-        .Baudrate = IO::CAN::DEFAULT_BAUD,
-        .Dict = hudl.getObjectDictionary(),
-        .DictLen = hudl.getObjectDictionarySize(),
-        .EmcyCode = NULL,
-        .TmrMem = appTmrMem,
-        .TmrNum = 16,
-        .TmrFreq = 1,
-        .Drv = &canStackDriver,
-        .SdoBuf = reinterpret_cast<uint8_t*>(&sdoBuffer[0]),
-    };
-
-    CO_NODE canNode;
-
-    CONodeInit(&canNode, &canSpec);
-    CONodeStart(&canNode);
-    CONmtSetMode(&canNode.Nmt, CO_OPERATIONAL);
-
-    time::wait(500);
-
-    //print any CANopen errors
-    log::LOGGER.log(log::Logger::LogLevel::DEBUG, "Error: %d\r\n", CONodeGetErr(&canNode));
-
-    hudl.initLCD();
-    uint8_t displayCounter = 0;
-    while (true) {
-        if (displayCounter >= 100) {
-            displayCounter = 0;
-            hudl.updateLCD();
-        } else {
-            displayCounter++;
-        }
-
-        CONodeProcess(&canNode);
-        // Update the state of timer based events
-        COTmrService(&canNode.Tmr);
-        // Handle executing timer events that have elapsed
-        COTmrProcess(&canNode.Tmr);
-        // Wait for new data to come in
-        //        time::wait(10);
-    }
-}
