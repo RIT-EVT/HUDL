48449843fa5819e0d5c4d5f51b3f163c1918e1a8
mjmagee991 <mjmagee991@gmail.com>
2024-05-07 16:23:57 -0400

    Applied Formatting Standards

--- include/HUDL.hpp
+++ include/HUDL.hpp
@@ -194,9 +194,9 @@ private:
         DATA_LINK_21XX(0, 4, CO_TUNSIGNED16, &thermTemps[3]),// Temperature Four
 
         DATA_LINK_START_KEY_21XX(1, 3),
-        DATA_LINK_21XX(1, 1, CO_TUNSIGNED16, &statusWord),    // Status Word
+        DATA_LINK_21XX(1, 1, CO_TUNSIGNED16, &statusWord),  // Status Word
         DATA_LINK_21XX(1, 2, CO_TSIGNED16, &actualPosition),// Actual Position
-        DATA_LINK_21XX(1, 3, CO_TUNSIGNED16, &torqueActual),  // Torque Actual
+        DATA_LINK_21XX(1, 3, CO_TUNSIGNED16, &torqueActual),// Torque Actual
 
         DATA_LINK_START_KEY_21XX(2, 2),
         DATA_LINK_21XX(2, 1, CO_TUNSIGNED16, &dummyValue),  // Unused Value
